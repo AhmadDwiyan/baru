edcfcb4809cf7845de6c191805e9eccfa3eb2ccf
AhmadDwiyan <41880033+AhmadDwiyan@users.noreply.github.com>
2019-10-09 07:30:23 -0500

    latihan react 2

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
-import Alert from ',/Component/Alert';
+import Alert from './Components/Alert';
+// import alert yang telah dibuat
 
 class App extends Component {
   render(){
     return (
-      <div className="App container">
-          <div className="alert alert-info">
-            <h3 className="text-danger">Ini project Pertama React </h3>
-            <p>Belajar React JS itu mudah</p>
-            <button className="mir-1 btn btn-success">Setuju</button>
-            <button className="btn btn-info">Iya dong!</button>
-          </div>
+      <div className="App container col-sm-6">
+          <Alert />
+          <Alert />
+          <Alert />
       </div>
     );
   }
